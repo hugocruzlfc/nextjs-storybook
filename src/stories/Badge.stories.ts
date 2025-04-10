eeedb1d9c6f2898678e231c330e8d08b564c3c23
Hugo Cruz <hugocruzlfc@gmail.com>
2025-04-10 12:25:39 +0200

    style: add badge story

---
+++ src/stories/Badge.stories.ts
@@ -0,0 +1,48 @@
+import { Badge } from "@/components/ui/badge";
+import type { Meta, StoryObj } from "@storybook/react";
+import "@/app/globals.css";
+
+const meta = {
+  title: "Example/Badge",
+  component: Badge,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+} satisfies Meta<typeof Badge>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const DefaultVariant: Story = {
+  args: {
+    variant: "default",
+    children: "Default",
+  },
+};
+
+export const SecondaryVariant: Story = {
+  args: {
+    variant: "secondary",
+    children: "Secondary",
+  },
+};
+
+export const DestructiveVariant: Story = {
+  args: {
+    variant: "destructive",
+    children: "Destructive",
+  },
+};
+
+export const OutlineVariant: Story = {
+  args: {
+    variant: "outline",
+    children: "Outline",
+  },
+};
